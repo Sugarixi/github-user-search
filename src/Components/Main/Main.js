ab909f0d870c9f6db3c28afa4dad161c9483f46e
DESKTOP-PITDT36\Rixi <62876717+Sugarixi@users.noreply.github.com>
2022-04-27 20:16:24 +0300

    added responsiveness

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -9,12 +9,12 @@ export default function Main(props) {
       <div className={Styles.userImgContainer}>
         <img className={Styles.userImage} src={props.user.avatar_url}></img>
       </div>
-      <div className={Styles.container}>
-        <div className={Styles.topMain}>
-          <div className={Styles.userInfo}>
-            <h1 className={Styles.name}>{props.user.login}</h1>
-            <h3 className={Styles.user}>@{props.user.login}</h3>
-          </div>
+
+      <div className={Styles.topMain}>
+        <div className={Styles.userInfo}>
+          <h1 className={Styles.name}>{props.user.login}</h1>
+          <h3 className={Styles.user}>@{props.user.login}</h3>
+
           <span className={Styles.joinedDate}>
             Joined{" "}
             {new Date(props.user.created_at).toLocaleDateString("en-GB", {
@@ -24,88 +24,88 @@ export default function Main(props) {
             })}
           </span>
         </div>
-        <span className={Styles.bio}>
-          {props.user.bio != null ? props.user.bio : "This profile has no bio"}
-        </span>
-        <div className={Styles.stats}>
-          <div>
-            <h4>Repos</h4>
-            <h2>{props.user.public_repos}</h2>
+      </div>
+      <span className={Styles.bio}>
+        {props.user.bio != null ? props.user.bio : "This profile has no bio"}
+      </span>
+      <div className={Styles.stats}>
+        <div className={Styles.statsItems}>
+          <h4>Repos</h4>
+          <h2>{props.user.public_repos}</h2>
+        </div>
+        <div className={Styles.statsItems}>
+          <h4>Followers</h4>
+          <h2>{props.user.followers}</h2>
+        </div>
+        <div className={Styles.statsItems}>
+          <h4>Following</h4>
+          <h2>{props.user.following}</h2>
+        </div>
+      </div>
+      <div className={Styles.infoContainer}>
+        <div className={Styles.firstInfoWrapper}>
+          <div className={Styles.info}>
+            <FontAwesomeIcon icon={solid("location-dot")} />{" "}
+            <span
+              style={{
+                opacity: props.user.twitter_username === null ? "0.5" : "",
+              }}
+            >
+              {props.user.location != null
+                ? props.user.location
+                : "Not Available"}
+            </span>
           </div>
-          <div>
-            <h4>Followers</h4>
-            <h2>{props.user.followers}</h2>
-          </div>
-          <div>
-            <h4>Following</h4>
-            <h2>{props.user.following}</h2>
+          <div className={Styles.info}>
+            <FontAwesomeIcon icon={brands("twitter")} />{" "}
+            {props.user.twitter_username != null ? (
+              <a
+                href={"https://twitter.com/" + props.user.twitter_username}
+                target="_blank"
+              >
+                <span>{props.user.twitter_username}</span>
+              </a>
+            ) : (
+              <span
+                style={{
+                  opacity: props.user.twitter_username === null ? "0.5" : "",
+                }}
+              >
+                {props.user.twitter_username != null
+                  ? props.user.twitter_username
+                  : "Not Available"}
+              </span>
+            )}
           </div>
         </div>
-        <div className={Styles.infoContainer}>
-          <div className={Styles.firstInfoWrapper}>
-            <div className={Styles.info}>
-              <FontAwesomeIcon icon={solid("location-dot")} />{" "}
+        <div>
+          <div className={Styles.info}>
+            <FontAwesomeIcon icon={solid("link")} />{" "}
+            {props.user.blog != "" ? (
+              <a href={props.user.blog} target="_blank">
+                <span>{props.user.blog}</span>
+              </a>
+            ) : (
               <span
                 style={{
                   opacity: props.user.twitter_username === null ? "0.5" : "",
                 }}
               >
-                {props.user.location != null
-                  ? props.user.location
-                  : "Not Available"}
+                {props.user.blog != "" ? props.user.blog : "Not Available"}
               </span>
-            </div>
-            <div className={Styles.info}>
-              <FontAwesomeIcon icon={brands("twitter")} />{" "}
-              {props.user.twitter_username != null ? (
-                <a
-                  href={"https://twitter.com/" + props.user.twitter_username}
-                  target="_blank"
-                >
-                  <span>{props.user.twitter_username}</span>
-                </a>
-              ) : (
-                <span
-                  style={{
-                    opacity: props.user.twitter_username === null ? "0.5" : "",
-                  }}
-                >
-                  {props.user.twitter_username != null
-                    ? props.user.twitter_username
-                    : "Not Available"}
-                </span>
-              )}
-            </div>
+            )}
           </div>
-          <div>
-            <div className={Styles.info}>
-              <FontAwesomeIcon icon={solid("link")} />{" "}
-              {props.user.blog != "" ? (
-                <a href={props.user.blog} target="_blank">
-                  <span>{props.user.blog}</span>
-                </a>
-              ) : (
-                <span
-                  style={{
-                    opacity: props.user.twitter_username === null ? "0.5" : "",
-                  }}
-                >
-                  {props.user.blog != "" ? props.user.blog : "Not Available"}
-                </span>
-              )}
-            </div>
-            <div className={Styles.info}>
-              <FontAwesomeIcon icon={solid("building-user")} />{" "}
-              <span
-                style={{
-                  opacity: props.user.twitter_username === null ? "0.5" : "",
-                }}
-              >
-                {props.user.company != null
-                  ? props.user.company
-                  : "Not Available"}
-              </span>
-            </div>
+          <div className={Styles.info}>
+            <FontAwesomeIcon icon={solid("building-user")} />{" "}
+            <span
+              style={{
+                opacity: props.user.twitter_username === null ? "0.5" : "",
+              }}
+            >
+              {props.user.company != null
+                ? props.user.company
+                : "Not Available"}
+            </span>
           </div>
         </div>
       </div>
